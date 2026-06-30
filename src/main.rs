0475816592d3c30d691e804558a7fc07db8f68d5
anrunt <75736769+anrunt@users.noreply.github.com>
2026-07-01 00:54:30 +0200

    Added getting herdr agents info.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, process::{Command}};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
@@ -11,7 +11,27 @@ fn main() {
     if args[0] != "reload" {
         println!("Error: Unknown command");
         return;
-    } else {
-        println!("Correct command!");
+    }
+
+    println!("Correct command!");
+
+    let herdr_path_result = env::var("HERDR_BIN_PATH");
+
+    let herdr_path = match herdr_path_result {
+      Ok(path) => path,
+      Err(_error) => String::from("herdr"),
+    };
+
+    println!("Path: {}", herdr_path);
+
+    match Command::new(herdr_path).args(["agent", "list"]).output() {
+      Ok(output) =>  {
+        if output.status.success() {
+          println!("Herdr output: {}", String::from_utf8_lossy(&output.stdout));
+        } else {
+          println!("Herdr output error: {}", String::from_utf8_lossy(&output.stderr));
+        }
+      }
+      Err(error) => println!("Unexpected error: {}", error)
     }
 }
