8d3a050869d3a084674470112d27edbed1cabd6d
anrunt <grunt.janek@gmail.com>
2026-07-10 19:13:09 +0200

    Still working on it, added basic option rendering

--- src/tui.rs
+++ src/tui.rs
@@ -6,9 +6,17 @@ pub fn run() -> std::io::Result<()> {
     ratatui::run(app)
 }
 
+struct AppState {
+    selected: usize
+}
+
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    let state = AppState {
+        selected: 0
+    };
+
     loop {
-        terminal.draw(render)?;
+        terminal.draw(|frame| render(frame, &state))?;
 
         if crossterm::event::read()?.is_key_press() {
             break Ok(());
@@ -16,7 +24,7 @@ fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
     }
 }
 
-fn render(frame: &mut Frame) {
+fn render(frame: &mut Frame, state: &AppState) {
     let [main_area, footer_area]= Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
@@ -26,8 +34,20 @@ fn render(frame: &mut Frame) {
         ])
         .areas(frame.area());
 
+    let menu_text_options = [
+        "Reload all Pi",
+        "Reset all Pi"
+    ];
 
-    let main = Paragraph::new("TUI placeholder")
+    let menu_text = menu_text_options.iter().enumerate().map(|(i, line)| {
+        if i == state.selected {
+            format!("> {}", line)
+        } else {
+            format!("  {}", line)
+        }
+    }).collect::<Vec<String>>().join("\n");
+
+    let main = Paragraph::new(menu_text)
         .block(Block::bordered().title("Herdr Pi Reloader"));
 
     let footer = Paragraph::new("Press any key to quit");
